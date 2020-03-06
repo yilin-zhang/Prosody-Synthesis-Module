6ccd6c364f50882e53ac904ecd14bb560ec364e0
Yilin Zhang <yzhang0129@outlook.com>
2020-03-06 00:22:29 -0500

    Fix a bug of loading empty entry in config.yml.

--- python/config.py
+++ python/config.py
@@ -21,10 +21,14 @@ if os.path.exists(_yml_config_path):
         config = yaml.load(f, Loader=yaml.Loader)
         print('load config.yml')
         # OSC bridge settings
-        OSC_BRIDGE_IP = config['osc_bridge_ip']
-        OSC_BRIDGE_PORT = config['osc_bridge_port']
+        if (config.get('osc_bridge_ip')):
+            OSC_BRIDGE_IP = config['osc_bridge_ip']
+        if (config.get('osc_bridge_port')):
+            OSC_BRIDGE_PORT = config['osc_bridge_port']
 
         # SuperCollider Settings
-        SC_IP = config['sc_ip']
-        SC_PORT = config['sc_port']
+        if (config.get('sc_ip')):
+            SC_IP = config['sc_ip']
+        if (config.get('sc_port')):
+            SC_PORT = config['sc_port']
         print(SC_PORT)
